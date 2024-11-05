ab950891fac91b9a40291ff68d21ebc6b29a0006
mzayem <mzayemazam@gmail.com>
2024-11-05 15:50:33 +0500

    model class

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    code = models.CharField(max_length=30)
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    description = models.TextField()
+    
