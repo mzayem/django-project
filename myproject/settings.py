d58ee56b4e78e1fd8fa54636cba78455eba3ff6f
mzayem <mzayemazam@gmail.com>
2024-12-04 09:54:44 +0500

    updating template

--- myproject/settings.py
+++ myproject/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "myapp" / "static",  # Make sure this path matches your project structure
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
